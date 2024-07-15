16ae104928aac9038cf76c1d5c1f8d81122d0b2b
chilam <chilam.Lee@outlook.com>
2024-07-15 11:46:32 +0800

    init

---
+++ src/test/java/cn/chilam/algorithmpractice/AlgorithmPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chilam.algorithmpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
